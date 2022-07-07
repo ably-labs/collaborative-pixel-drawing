a60759f6251246e0179681ae95ab4911a4b62146
Marc Duiker <marcduiker@gmail.com>
2022-07-07 10:40:49 +0200

    Add Startup to create Ably instance

---
+++ api/Startup.cs
@@ -0,0 +1,19 @@
+using System;
+using AblyLabs.PubSub;
+using IO.Ably;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+namespace AblyLabs.PubSub
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            var ablyApiKey = Environment.GetEnvironmentVariable("ABLY_APIKEY");
+            var ablyClient = new AblyRest(ablyApiKey);
+            builder.Services.AddSingleton<IRestClient>(ablyClient);
+        }
+    }
+}
